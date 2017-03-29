fc7322057d2658b9d81b31760d85241491d523a3
Thomas Lombardi <thomas.lombardi@nokia.com>
2017-03-29 16:31:41 -0500

    checking for preset recipe id in reducer

--- src/reducers/recipe-reducer.js
+++ src/reducers/recipe-reducer.js
@@ -3,26 +3,31 @@ import _ from 'lodash';
 
 const initialState = {
 	recipes: [],
+	recipeId: '',
 	selectedRecipe: {}
 }
+
+const dummyRecipe = {
+	recipeName: 'dummy'
+}
+
 
 const recipeReducer = function(state = initialState, action) {
 	switch(action.type) {
 		case types.GET_RECIPES_SUCCESS:
-			return Object.assign({}, state, { recipes: action.recipes });
+			if (state.recipeId.length === 0) {
+				return Object.assign({}, state, { recipes: action.recipes });
+			} else {
+				let idx = _.findIndex(action.recipes, { '_id': state.recipeId });
+				let recipe = (idx > -1) ? action.recipes[idx] : dummyRecipe;
+				return Object.assign({}, state, { recipes: action.recipes, selectedRecipe: recipe });
+			}
+
 
 		case types.SELECT_RECIPE:
-			// console.log('select Recipe', action.recipeId);
-			let recipe = null;
 			let idx = _.findIndex(state.recipes, { '_id': action.recipeId});
-			if (idx > -1) {
-				recipe = state.recipes[idx];
-			} else {
-				// recipe = dummyRecipe;
-				recipe = state.recipes[0];
-				console.log(idx, state.recipes);
-			}
-			return Object.assign({}, state, { selectedRecipe: recipe });
+			let recipe = (idx > -1) ? state.recipes[idx] : dummyRecipe;
+			return Object.assign({}, state, { selectedRecipe: recipe, recipeId: action.recipeId });
 
 		default:
 			return state;
@@ -30,8 +35,3 @@ const recipeReducer = function(state = initialState, action) {
 }
 
 export default recipeReducer;
-
-
-const dummyRecipe = {
-	recipeName: 'dummy'
-}
